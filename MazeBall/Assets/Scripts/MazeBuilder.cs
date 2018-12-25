4c3712b399e5f76b602f04816745b83ebd989079
JLoeve <jianglufull@foxmail.com>
2018-12-25 10:49:02 +0800

    临时commit

---
+++ MazeBall/Assets/Scripts/MazeBuilder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.MazeCore;
+using System.Threading;
+
+
+public class MazeBuilder : MonoBehaviour, IMazeBuilder
+{
+    #region MonoBehaviour
+    public int mazeBuildWidth = 10;
+    public int mazeBuildHeight = 10;
+    public int mazeStartUnitPositionX = 0;
+    public int mazeStartUnitPositionY = 0;
+    public int mazeEndUnitPositionX = 9;
+    public int mazeEndUnitPositionY = 9;
+    public float mazeWallThick = 0.3f;
+    public float mazeWallHeight = 0.5f;
+    public float buildInterval = 0.1f;
+    public Camera mainCamera;
+    private Queue<MazeWall> buildWallsQueue;
+    private GameObject groundObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildWallsQueue = new Queue<MazeWall>();
+        var trim = new TrimGenerateMazeAlgoritnm(this);
+        trim.GenerateMaze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (BuildWallsQueue.Count > 0)
+        {
+            var wall = BuildWallsQueue.Dequeue();
+            if (wall.HasWall)
+            {
+                if (wall.LocalObject == null)
+                {
+                    wall.LocalObject = BuildOneWall(wall.LocalPosition, wall.LocalScale);
+                }
+                else
+                {
+                    wall.LocalObject.SetActive(true);
+                }
+            }
+            else
+            {
+                wall.LocalObject?.SetActive(false);
+            }
+
+        }
+
+    }
+    #endregion
+    #region IMazeBuilder
+    public MazeSize MazeBuildSize => new MazeSize(mazeBuildWidth, mazeBuildHeight);
+    public MazePosition MazeStartUnitPosition => new MazePosition(mazeStartUnitPositionX, mazeStartUnitPositionY);
+    public MazePosition MazeEndUnitPosition => new MazePosition(mazeEndUnitPositionX, mazeEndUnitPositionY);
+    public float MazeWallThick => mazeWallThick;
+    public float MazeWallHeight => mazeWallHeight;
+    public float BuildInterval => buildInterval;
+    public Queue<MazeWall> BuildWallsQueue => buildWallsQueue;
+
+    public GameObject BuildGround()
+    {
+        groundObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        groundObj.transform.localScale = new Vector3(MazeBuildSize.Width, 1, MazeBuildSize.Height);
+        groundObj.transform.parent = transform;
+        return groundObj;
+    }
+
+    public GameObject BuildOneWall(Vector3 wallPosition, Vector3 wallScale)
+    {
+        GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.transform.position = wallPosition;
+        wall.transform.localScale = wallScale;
+        wall.transform.parent = groundObj.transform;
+        return wall;
+    }
+
+    #endregion
+}
+
---
+++ MazeBall/Assets/Scripts/MazeCore/IMazeBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.MazeCore
+{
+    public interface IMazeBuilder
+    {
+        /// <summary>
+        /// 迷宫建造的尺寸
+        /// </summary>
+        MazeSize MazeBuildSize { get; }
+        /// <summary>
+        /// 迷宫开始单元位置
+        /// </summary>
+        MazePosition MazeStartUnitPosition { get; }
+        /// <summary>
+        /// 迷宫结束单元位置
+        /// </summary>
+        MazePosition MazeEndUnitPosition { get; }
+        /// <summary>
+        /// 迷宫墙的厚度
+        /// </summary>
+        float MazeWallThick { get; }
+        /// <summary>
+        /// 迷宫墙的高度
+        /// </summary>
+        float MazeWallHeight { get; }
+        /// <summary>
+        /// 建造墙的时间间隔，单位：秒
+        /// </summary>
+        float BuildInterval { get; }
+        /// <summary>
+        /// 建立墙的队列
+        /// </summary>
+        Queue<MazeWall> BuildWallsQueue { get; }
+        /// <summary>
+        /// 建造地面
+        /// </summary>
+        GameObject BuildGround();
+    }
+}
